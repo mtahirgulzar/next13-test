9c48b5c0670c7504444314f061fee236b0dc9fea
mtahirgulzar <tahir.gulzar@vigorant.com>
2023-08-17 11:24:03 -0700

    fetching-data

--- src/app/about/[slug]/page.js
+++ src/app/about/[slug]/page.js
@@ -1,12 +1,47 @@
-import SmileBright2 from '@/components/smileBright'
-import React from 'react'
+import React from "react";
+import { adminPath } from "@/utils/constants";
+import { FaqCards } from "@/components/faq";
 
-function index() {
-  return (
-    <div>
-        <SmileBright2 />
-    </div>
-  )
+const Projects = async () => {
+  try {
+    const res = await fetch(`${adminPath}/projects?populate=deep`);
+    return res.json();
+  } catch (error) {
+    console.log("err>>>>>>", error);
+  }
+};
+
+async function getData(slug) {
+  const res = await fetch(`${adminPath}/projects/${slug}?populate=deep`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
 }
 
-export default index
+export default async function Index({ params: { slug } }) {
+  const slugdata = await getData(slug);
+  const postdata = slugdata?.data?.attributes;
+  const projectsData = await Projects();
+
+  console.log("slugs data>>>>", postdata);
+
+  return (
+    <>
+      {/* <Hero data={postdata?.projectPages[0]} /> */}
+      {/* <MuslimCommunitySuicide
+        data={postdata && postdata?.projectPages && postdata?.projectPages[1]}
+      /> */}
+      {/* <UniqueChallengesCards
+        data={postdata?.projectPages[2]}
+        projectsData={projectsData}
+      /> */}
+      {/* <SupportSuicide data={postdata?.projectPages[3]} /> */}
+      {/* <AppointmentCta data={postdata?.projectPages[4]} /> */}
+      <FaqCards data={postdata?.projectPages[5]} />
+      {/* <SurveyForm data={postdata?.projectPages[6]} /> */}
+      {/* <Testimonial data={postdata?.Testimonial} /> */}
+      {/* <MMHIUCBerkeley data={postdata?.projectPages[7]} /> */}
+    </>
+  );
+}
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,17 +1,47 @@
-import React from "react";
-import Link from "next/link";
+// import React from "react";
+// import Link from "next/link";
 
-function About() {
+// function About() {
+//   return (
+//     <>
+//       <div>About</div>
+//       <Link href={`/about/test`}>
+//         <button className="px-[5px] border-[2px] border-black rounded-[2px] bg-blue-700 text-white ">
+//           click me
+//         </button>
+//       </Link>
+//     </>
+//   );
+// }
+
+// export default About;
+// import CommonHero from "@/common/ui/common-hero";
+// import Newsletter from "@/components/news-letter";
+import EmpoveringCommunity from "@/components/empowering-community";
+import { adminPath } from "@/utils/constants";
+import React from "react";
+
+const getData = async () => {
+  const res = await fetch(`${adminPath}/all-project?populate=deep`);
+  return res.json();
+};
+export const metadata = {
+  title: "ALL PROJECT | MARISTAN",
+  description: "Maristan by vigorant",
+};
+const ProjectData = async () => {
+  const res = await fetch(`${adminPath}/projects?populate=deep`);
+  return res.json();
+};
+export default async function Projects() {
+  const data = await getData();
+  const projectData = await ProjectData();
+  const pageData = data?.data?.attributes;
   return (
     <>
-      <div>About</div>
-      <Link href={`/about/test`}>
-        <button className="px-[5px] border-[2px] border-black rounded-[2px] bg-blue-700 text-white ">
-          click me
-        </button>
-      </Link>
+      {/* <CommonHero data={pageData?.Hero} /> */}
+      <EmpoveringCommunity data={pageData?.HeadingBar} projectData={projectData}/>
+      {/* <Newsletter data={pageData?.NewsLetter} /> */}
     </>
   );
 }
-
-export default About;
